647fe375b0947c3c39f1a1f2ba65311ddd3352d6
Emile DJIDA G <djidadevacc@gmail.com>
2020-05-03 23:58:31 +0100

    database reset script Circular ImportError fixed

--- cename/utils.py
+++ cename/utils.py
@@ -1,27 +1,4 @@
 from cename import db
-try:
-    from cename.models import Region
-except:
-    pass
-
-
-def prepare_db():
-    """Prepare the database with necessary entries"""
-    regions = {
-            'ADA': 'ADAMAWA', 
-            'CEN': 'CENTER', 
-            'EST': 'EAST', 
-            'FND': 'FAR NORTH', 
-            'LIT': 'LITORAL',
-            'NRD': 'NORD', 
-            'NDW': 'NORD WEST', 
-            'STH': 'SOUTH', 
-            'STW': 'SOUTH WEST', 
-            'WST': 'WEST'
-        }
-    for code in regions.keys():
-        db.session.add(Region(region_code=code, region_name=regions[code]))
-    db.session.commit()
 
 def format_date(dt):
     return "%d-%d-%d"%(dt.year, dt.month, dt.day)
--- db_reset.py
+++ db_reset.py
@@ -1,6 +1,25 @@
+from cename.models import Region
 from cename import db
-from cename.utils import prepare_db
 from test import data_generator
+
+
+def prepare_db():
+    """Prepare the database with necessary entries"""
+    regions = {
+            'ADA': 'ADAMAWA', 
+            'CEN': 'CENTER', 
+            'EST': 'EAST', 
+            'FND': 'FAR NORTH', 
+            'LIT': 'LITORAL',
+            'NRD': 'NORD', 
+            'NDW': 'NORD WEST', 
+            'STH': 'SOUTH', 
+            'STW': 'SOUTH WEST', 
+            'WST': 'WEST'
+        }
+    for code in regions.keys():
+        db.session.add(Region(region_code=code, region_name=regions[code]))
+    db.session.commit()
 
 if __name__ == "__main__":
     choice = input("Proceed with database complete reset? YES(Y)/NO(N)]: ").lower() 
